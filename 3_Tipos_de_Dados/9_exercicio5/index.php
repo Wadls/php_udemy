12ee7e9a883e1da5bc99467b8d5278a4b104acf0
Wadls <wadsondaniel9@gmail.com>
2026-01-28 07:18:59 -0300

    Dia 04

---
+++ 3_Tipos_de_Dados/9_exercicio5/index.php
@@ -0,0 +1,5 @@
+<?php
+    echo "Texto com aspas duplas 'E Simples'";
+    echo 'Texto com aspas simples "E Duplas"';
+    //A Alternância entre aspas simples e duplas, permite que inclusão dessas aspas no texto
+    //Porém, lembre-se que a presença de aspas simples impede a concatenação com variáveis
